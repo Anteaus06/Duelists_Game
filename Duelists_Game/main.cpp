b833a9e05ad04e6c98c39eb15975bb888c9d54bc
Nelson <146144141+Anteaus06@users.noreply.github.com>
2025-05-08 19:49:57 -0700

    bugs fixing raylib

--- Duelists_Game/main.cpp
+++ Duelists_Game/main.cpp
@@ -1,242 +1,201 @@
-﻿/*******************************************************************************************
-*
-* raylib[core] example - Basic window
-*
-*Example complexity rating : [★☆☆☆] 1 / 4
-*
-*Welcome to raylib!
-*
-*To test examples, just press F6 and execute 'raylib_compile_execute' script
-* Note that compiled executable is placed in the same folder as.c file
-*
-*To test the examples on Web, press F6 and execute 'raylib_compile_execute_web' script
-* Web version of the program is generated in the same folder as.c file
-*
-*You can find all basic examples on C : \raylib\raylib\examples folder or
-*raylib official webpage : www.raylib.com
-*
-*Enjoy using raylib. : )
-*
-* Example originally created with raylib 1.0, last time updated with raylib 1.0
-*
-*Example licensed under an unmodified zlib / libpng license, which is an OSI - certified,
-* BSD - like license that allows static linking with closed source software
-*
-*Copyright(c) 2013 - 2025 Ramon Santamaria(@raysan5)
-*
-********************************************************************************************/
+﻿
 
-#include "raylib.h"
-
-//------------------------------------------------------------------------------------
-// Program main entry point
-//------------------------------------------------------------------------------------
-int main(void)
-{
-	// Initialization
-	//--------------------------------------------------------------------------------------
-	const int screenWidth = 800;
-	const int screenHeight = 450;
-
-	InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-
-	SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-	//--------------------------------------------------------------------------------------
-
-	// Main game loop
-	while (!WindowShouldClose())    // Detect window close button or ESC key
-	{
-		// Update
-		//----------------------------------------------------------------------------------
-		// TODO: Update your variables here
-		//----------------------------------------------------------------------------------
-
-		// Draw
-		//----------------------------------------------------------------------------------
-		BeginDrawing();
-
-		ClearBackground(RAYWHITE);
-
-		DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
-		EndDrawing();
-		//----------------------------------------------------------------------------------
-	}
-
-	// De-Initialization
-	//--------------------------------------------------------------------------------------
-	CloseWindow();        // Close window and OpenGL context
-	//--------------------------------------------------------------------------------------
-
-	return 0;
-}
-
-
-
-
-
-
-
-/*
 //duelingGame
+#include "raylib.h"
 #include <iostream>
-
 #include "Player.h"
 #include "Enemy.h"
-#include "Character.h"
-#include "Log.h"
+//#include "Character.h"
 
 
 void ProcessOutcome(Player& MainPlayer, Enemy& MainEnemy);
 
-int main()
+int main(void)
 {
+
+	//======= RAYLIB Initialization ============
+	const int screenWidth = 800;
+	const int screenHeight = 600;
+	InitWindow(screenWidth, screenHeight, "Adams Temptation");
+	SetTargetFPS(60);
+	//===== Game Init ==========================
+
 	Player MainPlayer(5, 2, 2, 2, "Adam");
-	Enemy MainEnemy(1, 1, 1, 1, "Lady"); 
+	Enemy MainEnemy(1, 1, 1, 1, "Lady");
 	int RoundNumber = 1;
 	int Heal = 1;
+	bool GameOver = false;
 
-	while (MainPlayer.GetIsAlive())
+	//	DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+	while (!WindowShouldClose())    // Detect window close button or ESC key
 	{
+		// Refreshes screen every loop
 
-		Log::Separator();
-		Log::LogMessage(LOG_DEFAULT, "Round " + std::to_string(RoundNumber));
-		Log::Separator();
-		Log::NewLine();
+		BeginDrawing();      //~~~~ Begin frame rendering  ~~~~~~~//
+		ClearBackground(BLACK); // CLEAR THE BACKGROUND
 
+		if (GameOver)
+		{
+
+			DrawText("Game Over. Press ESC to exit. ", 190, 200, 20, LIGHTGRAY);
+			EndDrawing();                   // Finalizes frame rendering
+			continue;
+
+		}
+
+		DrawText(("Round " + std::to_string(RoundNumber)).c_str(), 10, 15, 20, LIGHTGRAY);
 		ProcessOutcome(MainPlayer, MainEnemy);
 
-		
-	
-		static bool executed = false;
+		/*----------------------------- MY CODE FOR DROPING AN APPLE ------------------------------------
+			static bool executed = false;
 
 		if (RoundNumber == 3 && !executed)
-		{
-			Log::LogMessage(LOG_INFO, MainEnemy.GetName() + " Dropped apple, Adam ate it and healed");
-			MainPlayer.UpdateHealth(Heal);
-			executed = true;
-		}
-		
+		//	{
+				DrawText((MainEnemy.GetName() + " Dropped apple, Adam ate it and healed").c_str(), 190, 200, 20, LIGHTGRAY);
+				MainPlayer.UpdateHealth(Heal);
+				executed = true;
+		//	}
+		*/
 		if (!MainEnemy.GetIsAlive())
 		{
-			
-			RoundNumber += 1;
-	
+
+			RoundNumber++; // += 1; WAS ORIGINAL SUPPOSDELY THE SAME OUTCOME
+
 			if (RoundNumber > 5)
 			{
-				Log::LogMessage(LOG_INFO, MainEnemy.GetName() + " bows down! Man Rules ");
-				break;
+				DrawText((MainEnemy.GetName() + " bows down! Man Rules ").c_str(), 190, 200, 20, GREEN);
+				GameOver = true;
 			}
-			Log::LogMessage(LOG_DEFAULT, MainEnemy.GetName() + " has been defeated. A new girl appears " );
-			Log::NewLine();
+			DrawText((MainEnemy.GetName() + " has been defeated. A new girl appears ").c_str(), 190, 200, 20, LIGHTGRAY);
+
 
 
 			MainEnemy.IncreaseDifficulty(RoundNumber);
-			Log::LogMessage(LOG_WARNING, MainEnemy.GetName() + " looks mad that you beat her sister");
-			Log::NewLine();
+			DrawText((MainEnemy.GetName() + " looks mad that you beat her sister").c_str(), 190, 200, 20, LIGHTGRAY);
 			MainPlayer.InitStats();
-			Log::LogMessage(LOG_WARNING, MainEnemy.GetName() + " glares while, Adam Recovers his stamina ready for the next...");
-		
-			
+			DrawText((MainEnemy.GetName() + " glares while, Adam Recovers his stamina ready for the next...").c_str(), 190, 200, 20, GREEN);
+
 		}
-		
-				
+
+		if (!MainPlayer.GetIsAlive())
+		{
+			DrawText("Adam fell to sin ", 190, 200, 20, RED);
+			GameOver = true;
+		}
+		EndDrawing();
+
 	}
-	
-	if (!MainPlayer.GetIsAlive())
-	{
-		Log::LogMessage (LOG_ERROR,  "Adam fell to sin " );
-	}
-		
-	Log::LogMessage (LOG_ERROR, " Game Over  " );
+
+
+	// De-Initialization
+	CloseWindow();
 	return 0;
 
-	
-
-}
-	
-
-void ProcessOutcome(Player& MainPlayer, Enemy& MainEnemy)
-{
-
-	// process round based on actions
-	Action PlayerAction = MainPlayer.ChooseAction();
-	Action EnemyAction = MainEnemy.ChooseAction();
-
-	std::string PlayerActionStr = (PlayerAction == ATTACK) ? "Attack" : (PlayerAction == DEFEND) ? "Defend " : "Parry";
-	std::string EnemyActionStr = (EnemyAction == ATTACK) ? "Attack" : (EnemyAction == DEFEND) ? "Defend " : "Parry";
-
-	Log::NewLine();
-	Log::LogMessage (LOG_INFO, "Adam " + PlayerActionStr + "s" );
-	Log::LogMessage (LOG_WARNING, "She " + EnemyActionStr + "s" );
-	Log::NewLine();
-
-	switch (PlayerAction)
+	void ProcessOutcome(Player & MainPlayer, Enemy & MainEnemy)
 	{
+
+		// process round based on actions
+		Action PlayerAction = MainPlayer.ChooseAction();
+		Action EnemyAction = MainEnemy.ChooseAction();
+
+		std::string PlayerActionStr = (PlayerAction == ATTACK) ? "Attack" : (PlayerAction == DEFEND) ? "Defend " : "Parry";
+		std::string EnemyActionStr = (EnemyAction == ATTACK) ? "Attack" : (EnemyAction == DEFEND) ? "Defend " : "Parry";
+
+		// Display player and enemy actions
+		DrawText(("Adam " + PlayerActionStr + "s").c_str(), 10, 40, 20, LIGHTGRAY);
+		DrawText(("She " + EnemyActionStr + "s").c_str(), 10, 60, 20, LIGHTGRAY);
+
+		switch (PlayerAction)
+		{
 		case ATTACK:
 			switch (EnemyAction)
 			{
 			case ATTACK:
-				Log::LogMessage(LOG_DEFAULT, "Clash! wow close one ");
+				DrawText("Clash! wow close one ", 10, 120, 20, DARKGRAY);
 				break;
 
 			case PARRY:
-				Log::LogMessage(LOG_ERROR, MainEnemy.GetName() + " Parries the attack! We got hurt ");
+				DrawText((MainEnemy.GetName() + " Parries the attack! We got hurt ").c_str(), 10, 120, 20, RED);
 				MainPlayer.UpdateHealth(-(MainEnemy.GetAtkPower() * 2));
 				break;
 
 			case DEFEND:
-			Log::LogMessage(LOG_DEFAULT, MainEnemy.GetName() + " Blocked attack, took less damage");
-			MainEnemy.UpdateHealth(-(MainPlayer.GetAtkPower() / 2));
-			break;
+				DrawText((MainEnemy.GetName() + " Blocked attack, took less damage").c_str(), 10, 120, 20, LIGHTGRAY);
+				MainEnemy.UpdateHealth(-(MainPlayer.GetAtkPower() / 2));
+				break;
 			}
-		break;
-
-	case PARRY:
-		switch (EnemyAction)
-		{
-		case ATTACK:
-			Log::LogMessage(LOG_INFO, "Adam Parries and Hurt her ");
-			MainEnemy.UpdateHealth(-(MainPlayer.GetAtkPower() * 2));
 			break;
 
-		//	case PARRY:
-		//	Log::LogMessage(LOG_DEFAULT, "Nothing Happened ");========== MY VERSION OF PARRY before RECOVER METHOD
-		//	break;
+		case PARRY:
+
+			switch (EnemyAction)
+			{
+			case ATTACK:
+				DrawText("Adam Parries and Hurt her ", 190, 200, 20, LIGHTGRAY);
+				MainEnemy.UpdateHealth(-(MainPlayer.GetAtkPower() * 2));
+				break;
+
 			case PARRY:
-				Log::LogMessage(LOG_DEFAULT, "Both lost stamina by Parrying");
+				DrawText(("Both lost stamina by Parrying").c_str(), 190, 200, 20, LIGHTGRAY);
 			case DEFEND:
 				break;
-				Log::LogMessage(LOG_DEFAULT, "Adam loses Stamina while the Enemy recovers!");
-			//MainPlayer.UpdateHealth(-(MainEnemy.GetAtkPower() * 2));
-			break;
-		
-		}
-		break;
+				DrawText("Adam loses Stamina while the Enemy recovers!", 190, 200, 20, LIGHTGRAY);
+				//MainPlayer.UpdateHealth(-(MainEnemy.GetAtkPower() * 2));
+				break;
 
-	case DEFEND:
-		switch (EnemyAction)
-		{
-		case ATTACK:
-			Log::LogMessage(LOG_WARNING, "Adam defended against the attack! took little damage");
-			MainPlayer.UpdateHealth(-(MainEnemy.GetAtkPower() + 1));
+			}
 			break;
+
+		case DEFEND:
+			switch (EnemyAction)
+			{
+			case ATTACK:
+				DrawText("Adam defended against the attack! took little damage", 190, 200, 20, LIGHTGRAY);
+				MainPlayer.UpdateHealth(-(MainEnemy.GetAtkPower() + 1));
+				break;
+
+			case PARRY:
+				DrawText("Enemy loses Stamina while Adam Recovers ", 190, 200, 20, LIGHTGRAY);
+				break;
+			case DEFEND:
+				DrawText("both are recovering ", 190, 200, 20, LIGHTGRAY);
+				break;
+			}
+
+			break;
+		}
+
+	}
+}
+
+/*
+int main()
+{
+
+	while (MainPlayer.GetIsAlive())
+	{
+
+
+		
+	
+		
+				
+	}
+	
+		
+	DrawText (" Game Over  ", 190, 200, 20, LIGHTGRAY);
+	return 0;
+
+	
+
+}*/
+
+		//	case PARRY:
+		//	DrawText(LOG_DEFAULT, "Nothing Happened ");========== MY VERSION OF PARRY before RECOVER METHOD
+		//	break;
 
 		//case PARRY:===================== My version of PARRY
-		//	Log::LogMessage(LOG_INFO, "Adam blocked her Parry and it stung her, now she'll think twice ");
+		//	DrawText(LOG_INFO, "Adam blocked her Parry and it stung her, now she'll think twice ");
 		//	MainEnemy.UpdateHealth(-(MainPlayer.GetAtkPower() * 2));
 		//	break;
-		case PARRY:
-			Log::LogMessage(LOG_DEFAULT, "Enemy loses Stamina while Adam Recovers ");
-			break;
-		case DEFEND:
-			Log::LogMessage(LOG_DEFAULT, "both are recovering ");
-			break;
-		}
-
-		break;
-	}
-
-	Log::NewLine();
-}
-*/
